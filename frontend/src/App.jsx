82771b7216a61bffae842be9a722f833962566ea
vi1ka2 <129819852+vi1ka2@users.noreply.github.com>
2025-03-02 17:23:03 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   }, []);
 
   async function reviewCode(){
-      const response = await axios.post("//http://localhost:3000/ai/get-review/", {code});
+      const response = await axios.post("https://genai-mern-project-1.onrender.com", {code});
       setReview(response.data);
   }
 
